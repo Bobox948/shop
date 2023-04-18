cba6d06d774f1d486ff2b44327f0cdc0123c7eda
Boris <boris.pietrzak@gmail.com>
2023-04-18 12:12:29 +0200

    modified to get ready for gh pages portfolio

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,29 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-    return (
-      <div>
-
-
-<nav className="navbar navbar-expand-sm navbar-light bg-light">
-  <div className="container-fluid">
-  
-    <div id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link" href="/">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/products">Products</a>
-        </li>
-        
-      
-      </ul>
+  return (
+    <div>
+      <nav className="navbar navbar-expand-sm navbar-light bg-light">
+        <div className="container-fluid">
+          <div id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/products">
+                  Products
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
-  </div>
-</nav>
+  );
+};
 
-      </div>
-    );
-  };
-  
-  export default Navbar;
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import RouteSwitch from "./RouteSwitch";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import Products from './Products';
+import Navbar from './Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouteSwitch />
+    <HashRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
